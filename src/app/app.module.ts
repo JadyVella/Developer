f4015f2b37b75dedb1890d893e08dcb772a3ff74
Jady <felixouma379@gmail.com>
2020-01-22 12:05:39 +0300

    Created a form called developers-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { DevelopersComponent } from './developers/developers.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DevelopersService } from './developers-service/developers.service';
+import { DevelopersFormComponent } from './developers-form/developers-form.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevelopersComponent
+    DevelopersComponent,
+    DevelopersFormComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
